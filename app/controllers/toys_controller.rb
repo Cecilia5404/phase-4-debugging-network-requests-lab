3286384a1973646ab4cc3204ab8d79364a6bc40b
Cecil-Anyega <ceciliaanyega@gmail.com>
2022-10-02 00:01:03 +0300

    done

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,6 +1,5 @@
 class ToysController < ApplicationController
   wrap_parameters format: []
-
   def index
     toys = Toy.all
     render json: toys
@@ -8,6 +7,7 @@ class ToysController < ApplicationController
 
   def create
     toy = Toys.create(toy_params)
+    toy = Toy.create!(toy_params)
     render json: toy, status: :created
   end
 
@@ -15,6 +15,13 @@ class ToysController < ApplicationController
     toy = Toy.find_by(id: params[:id])
     toy.update(toy_params)
   end
+  # def update
+  #   toy = Toy.find_by(id: params[:id])
+  #   toy.update(toy_params)
+  #   toy.update(likes: params[:likes])
+  # end
+
+  # def destroy
 
   def destroy
     toy = Toy.find_by(id: params[:id])
@@ -24,8 +31,12 @@ class ToysController < ApplicationController
 
   private
   
+
   def toy_params
     params.permit(:name, :image, :likes)
+    params.permit(:name, :image, :likes, :id)
   end
 
 end
+
+
